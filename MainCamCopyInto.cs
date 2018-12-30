610b3807b331646df6899e7696edff7000cc9189
elvisish <steven@starar.net>
2018-12-30 10:40:58 +0000

    Add files via upload

---
+++ MainCamCopyInto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+class MainCamCopyInto : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+}
